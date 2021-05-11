8879d06ed82aa11edb676ecccf49722d95d52323
Brian <bweloba@gmail.com>
2021-05-11 09:38:28 +0300

    Add Database rule

--- src/test/java/models/EndangeredTest.java
+++ src/test/java/models/EndangeredTest.java
@@ -1,15 +1,19 @@
 package models;
 
+import org.junit.Rule;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class EndangeredTest {
 
+    @Rule
+    public DatabaseRule databaseRule = new DatabaseRule();
+
     @Test
     public void save_insertsObjectIntoDatabase() {
         Endangered animal = new Endangered("White Rhino","Healthy","Old");
         animal.save();
-        assertTrue(Endangered.all().get(0).eqals(animal));
+        assertTrue(Endangered.getAll().get(0).equals(animal));
     }
 }
