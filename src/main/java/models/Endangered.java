e971cbc1b34d1b76b1a0d91eb09aad44f351c1d2
Brian <bweloba@gmail.com>
2021-05-11 12:23:04 +0300

    Fix indentation and remove update method

--- src/main/java/models/Endangered.java
+++ src/main/java/models/Endangered.java
@@ -4,56 +4,31 @@ import org.sql2o.Connection;
 
 import java.util.List;
 
-public class Endangered extends Animal{
+public class Endangered extends Animal {
     public static final String animalType = "Endangered";
 
-    public Endangered(String name, String health, String age){
+    public Endangered(String name, String health, String age) {
         this.name = name;
         this.health = health;
         this.age = age;
         this.type = animalType;
     }
 
-    public static Animal findById(int id) {
-        try(Connection con = DB.sql2o.open()) {
+    public static Endangered findById(int id) {
+        try (Connection con = DB.sql2o.open()) {
             String sql = "SELECT * FROM animals where id=:id";
             return con.createQuery(sql)
                     .addParameter("id", id)
-                    .executeAndFetchFirst(Animal.class);
-        }
-    }
-
-    public static void update(int id, String name) {
-        try(Connection con = DB.sql2o.open()) {
-            String sql = "UPDATE animals SET name = :name WHERE id = :id";
-            con.createQuery(sql)
-                    .addParameter("name", name)
-                    .addParameter("id", id)
-                    .throwOnMappingFailure(false)
-                    .executeUpdate();
+                    .executeAndFetchFirst(Endangered.class);
         }
     }
 
     public static List<Endangered> getAll() {
         String sql = "SELECT * FROM animals WHERE type = 'Endangered';";
-        try(Connection con = DB.sql2o.open()) {
+        try (Connection con = DB.sql2o.open()) {
             return con.createQuery(sql)
                     .throwOnMappingFailure(false)
                     .executeAndFetch(Endangered.class);
-        }
-    }
-
-    public void save() {
-        try(Connection con = DB.sql2o.open()) {
-            String sql = "INSERT INTO animals (name, type, health, age) VALUES (:name, :type, :health, :age)";
-            this.id = (int) con.createQuery(sql, true)
-                    .addParameter("name", this.name)
-                    .addParameter("type", this.type)
-                    .addParameter("health", this.health)
-                    .addParameter("age", this.age)
-                    .throwOnMappingFailure(false)
-                    .executeUpdate()
-                    .getKey();
         }
     }
 }
