15963929997b466a0a29d5f2721ae3ba889377dd
Brian <bweloba@gmail.com>
2021-05-15 06:12:38 +0300

    Update styling

--- src/main/java/models/DB.java
+++ src/main/java/models/DB.java
@@ -3,7 +3,7 @@ package models;
 import org.sql2o.Sql2o;
 
 public class DB {
-    //    public static Sql2o sql2o  = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "nyosh04", "nyongesa");
+//        public static Sql2o sql2o  = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "nyosh04", "nyongesa");
     static String connectionString = "jdbc:postgresql://ec2-52-21-153-207.compute-1.amazonaws.com:5432/dah4qv55jga49s"; //!
     public static Sql2o sql2o = new Sql2o(connectionString, "yhtjmpekdqpwgc", "9057958965ce4bf3bb1bec8174a6d64e47c89abd6442078768981144c5c566ad");
 }
